9c345fae3edffd8cc6b3c4e1257e6b72613078d7
Greg <Ghughes610@gmail.com>
2021-08-26 15:28:32 -0600

    started over

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.scss';
-import Navbar from './components/Navbar';
+import Contact from './components/Contact/Contact'
+import Intro from './components/Intro/Intro'
+import Portfolio from './components/Portfolio/Portfolio'
+import Testimonials from './components/Testimonials/Testimonials'
+import Topbar from './components/Topbar/Topbar'
+import Works from './components/Works/Works'
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Contact />
+      <Intro />
+      <Portfolio />
+      <Testimonials />
+      <Topbar />
+      <Works />
     </div>
   );
 }
