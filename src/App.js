6014bb996dfda3a2bcad168eb8bbe6ac2348af54
Greg <Ghughes610@gmail.com>
2021-08-30 08:44:24 -0600

    animated hamburger

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
+import { useState } from 'react'
 import Contact from './components/Contact/Contact'
 import Intro from './components/Intro/Intro'
 import Portfolio from './components/Portfolio/Portfolio'
 import Testimonials from './components/Testimonials/Testimonials'
 import Topbar from './components/Topbar/Topbar'
 import Works from './components/Works/Works'
+import Modal from 'react-modal'
 import './app.scss'
 
+
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <div className="App">
       <Topbar />
       <div className="sections">
-
         <Intro />
         <Portfolio />
         <Works />
         <Testimonials />
         <Contact />
-
       </div>
     </div>
   );
--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import './topbar.scss'
+import PersonOutlineRoundedIcon from '@material-ui/icons/PersonOutlineRounded';
+import MailOutlineRoundedIcon from '@material-ui/icons/MailOutlineRounded';
 
 const Topbar = () => {
     return (
-        <div className='topbar'>
+        <div className='topbar active'>
+
             <div className="wrapper">
+
                 <div className="left">
-                    <a href="#intro" className="logo">Intro</a>
+                        
+                    <a href="#intro" className="logo">Hughes</a>
+                    <div className="itemContainer">
+                            <PersonOutlineRoundedIcon classname="icon"/>
+                            <span classname="span">631-408-1224</span>
+                    </div>
+                    <div className="itemContainer">
+                        <MailOutlineRoundedIcon classname="icon" />
+                        <span classname="span">Ghughes610@gmail.com</span>
+                    </div>
                 </div>
+
                 <div className="right">
+                    <div className="hamburger">
+                        <span classname='line1'></span>
+                        <span classname='line2'></span>
+                        <span classname='line3'></span>
+                    </div>
                 </div>
+
             </div>
         </div>
-    )
+    );
 }
 
 export default Topbar
