1332225e82db0cef10e08ba12516f2ce0d97180a
Greg <Ghughes610@gmail.com>
2021-09-20 15:33:59 -0600

    finished intro page 1.0
    fixed the arrow and flexed the right container

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,6 +1,9 @@
-import {React, useEffect, useRef} from 'react'
+import { React, useEffect, useRef } from 'react'
 import './intro.scss'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { init } from 'ityped'
+import icons from '@material-ui/icons'
+import portfolio from '../Portfolio/Portfolio'
 
 const Intro = () => {
 
@@ -10,31 +13,43 @@ const Intro = () => {
     init(textRef.current, {
       showCursor: false,
       strings: [
-        "FullStack Software Developer",
+        "FullStack Software Engineer",
         "Web Developer",
         "Rock Climber",
-        "Learner"
-      ]
+        "Learner",
+        "Problem Solver"
+      ],
+      typeSpeed: 125,
+      startDelay: 1000,
+      loop: true
     })
-  },[])
+  }, [])
 
-    return (
-        <div className="intro" id="intro">
-          <div className="left">
-            <div className="imgContainer">
-              <img src="assets/IMG_0301.png" alt="profilePicture" className="profilePicture" />
-            </div>
-          </div>
-          <div className="right">
-            <div className="wrapper">
-              <h2>Hello there, I'm</h2>
-              <h1>Greg Hughes</h1>
-              <span ref={textRef}></span>
-                <a className="arrow" href="#portfolio"></a>
-            </div>
+  return (
+
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/IMG_0301.png" alt="profilePicture" className="profilePicture" />
+        </div>
+      </div>
+      <Router>
+        <div className="right">
+          <div className="wrapper">
+            <h2>Hello there, I'm</h2>
+            <h1>Greg Hughes</h1>
+            <Switch>
+              <Route path="/portfolio" exact>
+              </Route>
+            </Switch>
+            <span className="textRef"ref={textRef}></span>
+            <a href="#portfolio" className="arrow"></a>
           </div>
         </div>
-    )
+      </Router>
+    </div>
+
+  )
 }
 
 export default Intro
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './navBar.scss' 
+
+const NavBar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NavBar
